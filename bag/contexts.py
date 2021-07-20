3ee6b9eb0f5be7c3badc7a41fb9808f1cc62c9bd
AideenM12 <a.malone1289@gmail.com>
2021-07-20 14:14:07 +0000

    Create add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,7 +7,6 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-    
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -15,7 +14,7 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
 
     context = {
