06992d7f64b3c2ba929242fbaef37db05647f0c3
AideenM12 <a.malone1289@gmail.com>
2021-07-29 13:53:32 +0000

    Add crispy forms to checkout template

--- checkout/forms.py
+++ checkout/forms.py
@@ -9,9 +9,8 @@ class OrderForm(forms.ModelForm):
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county',)
-        
 
-        def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
