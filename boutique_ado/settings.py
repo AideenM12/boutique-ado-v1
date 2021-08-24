ba8247cdf9ec3e0a744bbbaecc99489ca7b42981
AideenM12 <a.malone1289@gmail.com>
2021-08-24 13:37:30 +0000

    Add DB var to settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -130,10 +130,8 @@ else:
     }
 
 
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+#  Password validation
+#  https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
